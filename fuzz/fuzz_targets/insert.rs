12add07d4d8a7171fb68a56219c3fa10a7af7529
Jonathanzhao02 <zhaojonathan99@gmail.com>
2023-05-08 18:44:41 +0000

    Added mayhem fuzzing

---
+++ fuzz/fuzz_targets/insert.rs
@@ -0,0 +1,11 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use patricia_merkle_tree::PatriciaMerkleTree;
+use sha3::Keccak256;
+
+fuzz_target!(|value: Vec<(String,String)>| {
+    let mut tree = PatriciaMerkleTree::<String, String, Keccak256>::new();
+    for pair in value {
+        tree.insert(pair.0, pair.1);
+    }
+});
