94b65a4da1728b98453d561573b9ba7dff0c84ae
Jonathanzhao02 <zhaojonathan99@gmail.com>
2023-05-08 19:10:43 +0000

    Fixed sort order

--- fuzz/fuzz_targets/hash.rs
+++ fuzz/fuzz_targets/hash.rs
@@ -5,7 +5,9 @@ use sha3::Keccak256;
 
 fuzz_target!(|value: Vec<(String,String)>| {
     let mut tree = PatriciaMerkleTree::<String, String, Keccak256>::new();
-    for pair in value {
+    let mut data = value;
+    data.sort_by(|a,b| a.0.len().cmp(&b.0.len()));
+    for pair in data {
         tree.insert(pair.0, pair.1);
     }
     tree.compute_hash();
